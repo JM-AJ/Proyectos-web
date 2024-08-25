2f0e85200acf42ce0f0cb336b0e95d4d18c3f25c
JM-AJ <jeremiaraujo1@gmail.com>
2024-08-25 12:50:41 -0400

    Add files via upload

---
+++ Practica1/main.js
@@ -0,0 +1,28 @@
+var variable = 'Hola mundo'
+var variable2 = 'Hasta pronto'
+
+let nombre = 'jeremi'; 
+let edad = 18; 
+let soltero = true; 
+let peso = 147.8; 
+let mas = {ocupacion:'estudiante', periodo:'7mo'}
+
+/*var para decalrar variable local
+    let para declarar variables de bloques
+*/
+/*
+document.write(variable, '<br>')
+document.write(nombre, '<br>')
+document.write(edad, '<br>')
+document.write('soltero> ', soltero, "<br>")
+document.write(mas.ocupacion, '<br>',mas.periodo, '<br>')
+document.write(variable2, "<br>")*/
+
+/* para captura de datos podemos utilizar el prompt*/
+/*let Nombre = prompt('Ingresa tu nombre: ')
+let Edad = prompt('Cual es su edad: ')
+document.write('Nombre: ', Nombre, '<br>', 'Edad: ', Edad)*/
+var ocultar; 
+if(InputEvent){
+
+}
